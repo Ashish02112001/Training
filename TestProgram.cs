3085c3ddc1399148bc1ae86f4cbd9dc9676b39a2
tmmvigneshm <vigneshm@metamation.com>
2023-07-24 15:57:05 +0530

    Reference for comments

---
+++ TestProgram.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2013.
+// Copyright (c) Metamation India.                                                ===  --  
+// ---------------------------------------------------------------------------      ==(  )
+// Program.cs                                                                     ===  --
+// <TODO: Define what this file going to contains in terms of functionalities.(min 1:max 4 lines).
+// e.g: Class program prints the text 'Hello World' in the console.
+// Likewise, if the class has more definitions of methods, then just explain the functionalities in simple words>
+// ---------------------------------------------------------------------------------------
+
+namespace Training {
+   #region TestProgram --------------------------------------------------------------------------
+   /// <summary>Class Program</summary>
+   internal class TestProgram {
+      #region Methods ---------------------------------------------
+      /// <summary>This method prints 'Hello, World!'.</summary>
+      /// <param name="args">arguments</param>
+      static void Main (string[] args) {
+         Console.WriteLine ("Hello, World!");
+      }
+      #endregion
+   }
+   #endregion
+}
