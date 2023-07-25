75375894f06a6b3f11260c670a9a2687fb64a04a
Ashish <ashishkumar.m.b@metamation.com>
2023-07-25 14:04:27 +0530

    Created a new branch for Assignment - A02

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿namespace Training {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int n = new Random ().Next (1, 101), guess;
+         Console.Write("Enter your guess between 1 to 100:");
+         for (; ; ) {
+            guess = int.Parse(Console.ReadLine());
+            if (guess == n) { Console.WriteLine ("You guessed correctly"); break;}
+            if (guess < n) { Console.WriteLine ("Your guess is too low"); }
+            if (guess > n) { Console.WriteLine ("Your guess is too high"); }
+             
+         } 
+         
       }
    }
 }
