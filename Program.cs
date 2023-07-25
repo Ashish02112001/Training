664de45988dc3cf0b8294ab31502988f845602f9
Ashish <ashishkumar.m.b@metamation.com>
2023-07-25 15:19:14 +0530

    Added Comments to the Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Training {
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2023.
+// Copyright (c) Metamation India.                                              
+// ------------------------------------------------------------------------
+// Program.cs
+// Simple Program to Print the text 'Hello, World!' in Console
+// --------------------------------------------------------------------------------------------
+
+
+namespace Training {
+   #region Program ------------------------------------------------------------------------------
+   /// <summary>Sample program</summary>
    internal class Program {
+      #region Methods ---------------------------------------------
+      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <param name="args">arguements</param>
       static void Main (string[] args) {
          Console.WriteLine ("Hello, World!");
       }
+      #endregion
    }
+   #endregion
 }
