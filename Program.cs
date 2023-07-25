bfa64211505412f86567755f8ebbaad9f28776a1
Ashish <ashishkumar.m.b@metamation.com>
2023-07-25 15:37:37 +0530

    Updated the Assignment branch A02 with comments

--- Program.cs
+++ Program.cs
@@ -1,16 +1,52 @@
-﻿namespace Training {
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2023.
+// Copyright (c) Metamation India.                                              
+// ------------------------------------------------------------------------
+// Program.cs
+// program to implement a simple guessing game :
+// The computer thinks of a random number between 1 and 100, and the user has to guess it.
+// The user can enter an number, and the computer will respond with one of these:
+// - Your guess is too high - Your guess is too low - You guessed correctly
+// --------------------------------------------------------------------------------------------
+
+
+namespace Training {
+   #region Program ------------------------------------------------------------------------------
+   /// <summary>simple guessing game</summary>
    internal class Program {
+      #region Methods ---------------------------------------------
+      /// <summary>This Method gets a random number and the user has to guess it</summary>
+      /// <param name="args">arguements</param>
       static void Main (string[] args) {
          int n = new Random ().Next (1, 101), guess;
-         Console.Write("Enter your guess between 1 to 100:");
+         Console.Write ("Enter your guess between 1 to 100:");
          for (; ; ) {
-            guess = int.Parse(Console.ReadLine());
-            if (guess == n) { Console.WriteLine ("You guessed correctly"); break;}
+            guess = int.Parse (Console.ReadLine ());
+            if (guess == n) { Console.WriteLine ("You guessed correctly"); break; }
             if (guess < n) { Console.WriteLine ("Your guess is too low"); }
             if (guess > n) { Console.WriteLine ("Your guess is too high"); }
-             
-         } 
-         
+
+         }
       }
+      #endregion
    }
+   #endregion
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
