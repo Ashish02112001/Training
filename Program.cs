247360f40e40d26cff9acc01dc2f5c03e8575e07
Ashish <ashishkumar.m.b@metamation.com>
2023-07-27 16:20:11 +0530

    Done the following Changes:
    Used some pretext when taking input. "> "
    Fixed the Default difficulty level to medium.
    Added comments for Methods & Enum.
    Removed Goto statements and made to print message when input is not valid.
    Placed the Methods outside of Main and inside class Program.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@
 // - Your guess is too high - Your guess is too low - You guessed correctly
 // --------------------------------------------------------------------------------------------
 
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>simple guessing game</summary>
@@ -18,46 +17,51 @@ namespace Training {
       /// <summary>This Method gets a random number and the user has to guess it</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
+         /// Main Method creates a random number and compares it with the guess of the user
          var mode = GetMode ();
          Console.WriteLine (mode);
          int max = GetMax (mode);
          int n = new Random ().Next (1, max + 1), guess = 0;
-         Again:
-         Console.Write ($"Enter your guess between 1 to {max}: ");
+         Console.WriteLine ($"Enter your guess between 1 to {max}: ");
          for (; ; ) {
+            Console.Write ("> ");
             if (int.TryParse (Console.ReadLine (), out guess)) {
-               if (guess == n) { Console.WriteLine ("You guessed correctly"); break; }
+               if (guess == n) { Console.WriteLine ("You guessed Correctly"); break; }
                if (guess < n) { Console.WriteLine ("Your guess is too low"); }
-               if (guess > n) { Console.WriteLine ("Your guess is too high"); } else { goto Again; }
-
-            }
-
+               if (guess > n) { Console.WriteLine ("Your guess is too high"); } 
+            } else { Console.WriteLine ("Enter a Valid number"); }
          }
-         Mode GetMode() {
-            Console.Write ("Select a mode (E)asy, (M)edium, (H)ard:");
-            for (; ; ) {
-               var key = Console.ReadKey (true).Key;
-               switch (key) {
-                  case ConsoleKey.E: return Mode.Easy;
-                  case ConsoleKey.M: return Mode.Medium;
-                  case ConsoleKey.H: return Mode.Hard;
-                  default : Console.WriteLine ("Incorrect Key"); break;
-               }   
-            }
-         }
-         int GetMax (Mode mode) {
-            switch (mode) {
-               case Mode.Easy: return 10;
-               case Mode.Hard: return 1000;
-               default: return 100; 
+      }
+
+     static Mode GetMode () {
+         /// GetMode gets the difficulty level from the user 
+         Console.Write ("Select a mode (E)asy, (M)edium, (H)ard:");
+         for (; ; ) {
+            var key = Console.ReadKey (true).Key;
+            switch (key) {
+               case ConsoleKey.E: return Mode.Easy;
+               case ConsoleKey.M: return Mode.Medium;
+               case ConsoleKey.H: return Mode.Hard;
+               default: return Mode.Medium;
             }
          }
       }
+     static int GetMax (Mode mode) {
+         /// GetMax gets sets the Maximum rang for the difficulty level selected by user
+         switch (mode) {
+            case Mode.Easy: return 10;
+            case Mode.Hard: return 1000;
+            default: return 100;
+         }
+      }
+      /// <summary> 3 Levels of difficulty is declared under enumeration data type </summary>
+      enum Mode { Easy, Medium, Hard }
       #endregion
    }
    #endregion
-   enum Mode { Easy, Medium, Hard }
 }
+
+
 
 
 
