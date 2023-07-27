dafee93ee209784284eb981b124a78d5853757bd
Ashish <ashishkumar.m.b@metamation.com>
2023-07-27 09:48:28 +0530

    Added the difficulty levels for the guessing game

--- Program.cs
+++ Program.cs
@@ -18,21 +18,45 @@ namespace Training {
       /// <summary>This Method gets a random number and the user has to guess it</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         int n = new Random ().Next (1, 101), guess = 0;
+         var mode = GetMode ();
+         Console.WriteLine (mode);
+         int max = GetMax (mode);
+         int n = new Random ().Next (1, max + 1), guess = 0;
          Again:
-         Console.Write ("Enter your guess between 1 to 100: ");
+         Console.Write ($"Enter your guess between 1 to {max}: ");
          for (; ; ) {
             if (int.TryParse (Console.ReadLine (), out guess)) {
                if (guess == n) { Console.WriteLine ("You guessed correctly"); break; }
                if (guess < n) { Console.WriteLine ("Your guess is too low"); }
-               if (guess > n) { Console.WriteLine ("Your guess is too high"); }
-            } else { goto Again; }
-            
+               if (guess > n) { Console.WriteLine ("Your guess is too high"); } else { goto Again; }
+
+            }
+
+         }
+         Mode GetMode() {
+            Console.Write ("Select a mode (E)asy, (M)edium, (H)ard:");
+            for (; ; ) {
+               var key = Console.ReadKey (true).Key;
+               switch (key) {
+                  case ConsoleKey.E: return Mode.Easy;
+                  case ConsoleKey.M: return Mode.Medium;
+                  case ConsoleKey.H: return Mode.Hard;
+                  default : Console.WriteLine ("Incorrect Key"); break;
+               }   
+            }
+         }
+         int GetMax (Mode mode) {
+            switch (mode) {
+               case Mode.Easy: return 10;
+               case Mode.Hard: return 1000;
+               default: return 100; 
+            }
          }
       }
       #endregion
    }
    #endregion
+   enum Mode { Easy, Medium, Hard }
 }
 
 
