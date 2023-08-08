ed4fa372d6cfbe59a794ed58e271909a2fc7c0f6
Ashish <ashishkumar.m.b@metamation.com>
2023-08-08 09:26:26 +0530

    Corrected the comments

--- Program.cs
+++ Program.cs
@@ -3,37 +3,25 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// program to convert a given decimal number to different forms (Hexadecimal, Binary)
+// and display the converted values in the console window. 
 // --------------------------------------------------------------------------------------------
 
 
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Decimal to binary and Hexadecimal Conversion</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>This Method converts the input number to binary and hexadecimal equivalent</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-
          Console.WriteLine ("\nEnter a number:");
          string num = Console.ReadLine ();
          int num1 = int.Parse (num);
          Console.WriteLine ($"The binary equivalent is {Convert.ToString(num1, 2)}");
          Console.WriteLine ($"The Hexadecimal equivalent is {num1:X}");
-         //Stack<int> binNum = new Stack<int> ();
-         //while (num1 > 0) {
-         //   binNum.Push (num1 % 2);
-         //   num1 /= 2;
-         //}
-         //Console.Write ("Binary Equivalent is : ");
-         //while (binNum.Count > 0) {
-         //   Console.Write(binNum.Pop ());
-         //}
-         //char[] HexD = { 'A', 'B', 'C', 'D', 'E', 'F' };
-
       }
-      
       #endregion
    }
    #endregion
