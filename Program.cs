6bc8764f0a68b33d2155c03b208a09cec786e685
Ashish <ashishkumar.m.b@metamation.com>
2023-08-08 09:20:29 +0530

    Created T01 - Decimal number to binary and Hexadecimal

--- Program.cs
+++ Program.cs
@@ -15,8 +15,25 @@ namespace Training {
       /// <summary>This Method prints "Hello, World!"</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+
+         Console.WriteLine ("\nEnter a number:");
+         string num = Console.ReadLine ();
+         int num1 = int.Parse (num);
+         Console.WriteLine ($"The binary equivalent is {Convert.ToString(num1, 2)}");
+         Console.WriteLine ($"The Hexadecimal equivalent is {num1:X}");
+         //Stack<int> binNum = new Stack<int> ();
+         //while (num1 > 0) {
+         //   binNum.Push (num1 % 2);
+         //   num1 /= 2;
+         //}
+         //Console.Write ("Binary Equivalent is : ");
+         //while (binNum.Count > 0) {
+         //   Console.Write(binNum.Pop ());
+         //}
+         //char[] HexD = { 'A', 'B', 'C', 'D', 'E', 'F' };
+
       }
+      
       #endregion
    }
    #endregion
