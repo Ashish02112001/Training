72d5490b269d1a6d6f34f945a4114ffcf07c6c2e
Ashish <ashishkumar.m.b@metamation.com>
2023-08-09 12:53:42 +0530

    Created Assignment T03

--- Program.cs
+++ Program.cs
@@ -3,19 +3,30 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// program to display the Fibonacci series given an input
 // --------------------------------------------------------------------------------------------
 
 
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Fibonaccci Series</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>User gives the length of the series as input for Fibonacci series to be printed</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.WriteLine ("Enter the length of Fibonacci series: ");
+         int end = int.Parse(Console.ReadLine());
+         Console.WriteLine("\n------Fibonacci Series------");
+         int count = 2, i = 0, j = 1;
+         Console.Write ($"{i} {j}");
+         while (count <= end) {
+            int result = i + j;
+            Console.Write ($" {result}");
+            i = j; j = result;
+            count++;
+         }
+         Console.WriteLine ();
       }
       #endregion
    }
