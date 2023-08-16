43e5de58aa53c8dded7a16c6a338843c6273d02e
Ashish <ashishkumar.m.b@metamation.com>
2023-08-16 14:54:28 +0530

    Completed T08 Multiplycation tables

--- Program.cs
+++ Program.cs
@@ -3,19 +3,23 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// Printing multiplication tables from 1 to 10 in a tabular form on a console with a line gap between each table.
 // --------------------------------------------------------------------------------------------
 
 
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Print Tables</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>Prints multiplycation table from 1 to 10</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         for (int tab = 1;tab <= 10; tab++) {
+            for (int num = 1; num <= 10;num++) 
+               Console.WriteLine ($"{tab} * {num,3} = {tab*num}");
+            Console.WriteLine ();
+         }
       }
       #endregion
    }
