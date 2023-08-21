d2197043655409835f6e8a86ef4047921f004033
Ashish <ashishkumar.m.b@metamation.com>
2023-08-21 17:45:02 +0530

    Made changes and completed A04

--- Program.cs
+++ Program.cs
@@ -18,15 +18,14 @@ namespace Training {
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
          string[] words = File.ReadAllLines ("C:/Training/words.txt");
-         Dictionary<char, int> lettersCount = new Dictionary<char, int> ();
+         Dictionary<char, int> lettersCount = new ();
          foreach (string word in words) {
             foreach (char ch in word) {
-               if (lettersCount.ContainsKey (ch)) lettersCount[ch]++;
-               else lettersCount.Add (ch, 1);
+               lettersCount[ch] = lettersCount.TryGetValue (ch, out int value) ? ++value : 1;
             }
          }
          int count = 0;
-         foreach (KeyValuePair<char, int> pair in lettersCount.OrderByDescending(a => a.Value)) {
+         foreach (var pair in lettersCount.OrderByDescending(a => a.Value)) {
             if (pair.Key >= 'A' && pair.Key <= 'Z') Console.WriteLine ($"{pair.Key} = {pair.Value}"); count++;
             if (count == 7) Console.WriteLine ("----------These 7 letters can be used for spell bee game----------");
          } 
