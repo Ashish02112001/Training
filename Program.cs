5688d9073318f4795dfcacc836bf5d542e876d5a
Ashish <ashishkumar.m.b@metamation.com>
2023-08-21 16:58:49 +0530

    Added Comments to T07

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+//  program to display the chess board with all pieces black and white which
+//  resembles an actual chess board in the console window using Unicode
 //250C - Left upper corner
 //2510 - right upper corner
 //2500 - hLine
@@ -22,10 +23,10 @@ using System.Text;
 
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Chess Board</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>Prints the Chess board and pieces</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
          Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -35,6 +36,8 @@ namespace Training {
          Console.Write ('\u2666');
          #endregion
       }
+      /// <summary>Prints the grids and pieces of chess board</summary>
+      /// <param name="row"></param>
       static void Lines (int row) {
          if (row == 0) Top();
          else if (row == 16) Bottom ();
@@ -44,7 +47,6 @@ namespace Training {
             for (int col = 1; col < 32; col++)
                if (col % 4 == 0) Console.Write ('\u2502');
                else if (row is 1 or 3 or 13 or 15  && Place (col)) PrintPiece (row, col);
-               //else if (row == 3 && Place (col)) PrintPiece (row, col);
                else if (row is not 3 or 13) Console.Write (" ");
             Console.Write ('\u2502');
             Console.WriteLine ();
@@ -59,6 +61,9 @@ namespace Training {
             Console.WriteLine ();
          }
       }
+      /// <summary>This method gives the position at which the piece is printed</summary>
+      /// <param name="n">coloumn position</param>
+      /// <returns>True to print in specific position</returns>
       static bool Place (int n) {
          for (int i = 1; i <= 8; i++) {
             int num = (4 * i) - 2;
@@ -66,6 +71,9 @@ namespace Training {
          }
          return false;
       }
+      /// <summary>This method prints the pieces at the specific position</summary>
+      /// <param name="area">Row of the chess board</param>
+      /// <param name="pos">Coloumn(position) of piece</param>
       static void PrintPiece (int area,int pos) {
          char[] BlackPiece = new char[] { '\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657', '\u2658', '\u2656' };
          char BlackSol = '\u2659';
@@ -75,7 +83,8 @@ namespace Training {
          else if (area == 3) Console.Write ($" {WhiteSol}");
          else if (area == 13) Console.Write ($"{BlackSol}");
          else if (area == 15) Console.Write ($"{BlackPiece[(pos + 2) / 4 - 1]}");
-      }  
+      }
+      /// <summary>Prints Bottom portion of chess board</summary>
       static void Bottom () {
          Console.Write ('\u2514');
          for (int col = 1; col < 32; col++) 
@@ -84,6 +93,7 @@ namespace Training {
          Console.Write ('\u2518');
          Console.WriteLine ();
       }
+      /// <summary>Prints Top portion of chess board</summary>
       static void Top () {
          Console.Write ('\u250C');
          for (int col = 1; col < 32; col++)
@@ -92,12 +102,6 @@ namespace Training {
          Console.Write ('\u2510');
          Console.WriteLine ();
       }
-      //static void PrintPawn (bool cl) {
-      //   char BlackSol = '\u2659';
-      //   char WhiteSol = '\u265F';
-      //   if(cl) Console.Write ($"{WhiteSol} |");
-      //   else Console.Write ($"{BlackSol} |);
-      //}
       #endregion
    }
 }
