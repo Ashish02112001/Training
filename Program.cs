b55db366660fc3217b2494b649b2142b1011491e
Ashish <ashishkumar.m.b@metamation.com>
2023-08-22 15:22:03 +0530

    Made changes in T01

--- Program.cs
+++ Program.cs
@@ -16,11 +16,14 @@ namespace Training {
       /// <summary>This Method converts the input number to binary and hexadecimal equivalent</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("\nEnter a number:");
-         string num = Console.ReadLine ();
-         int num1 = int.Parse (num);
-         Console.WriteLine ($"The binary equivalent is {Convert.ToString(num1, 2)}");
-         Console.WriteLine ($"The Hexadecimal equivalent is {num1:X}");
+         Console.Write ("\nEnter a number: ");
+         for (; ; )
+            if (int.TryParse (Console.ReadLine (), out int result)) {
+               Console.WriteLine ($"The binary equivalent is {Convert.ToString (result, 2)}");
+               Console.WriteLine ($"The Hexadecimal equivalent is {result:X}");
+               break;
+            }
+            else Console.Write ("Enter a valid number: ");
       }
       #endregion
    }
