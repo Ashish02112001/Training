615f54fbb946419d02f29ae48b39c440d5689a2c
Ashish <ashishkumar.m.b@metamation.com>
2023-08-24 12:47:28 +0530

    Made changes in T03 - Fibonacci series

--- Program.cs
+++ Program.cs
@@ -15,20 +15,26 @@ namespace Training {
       /// <summary>User gives the length of the series as input for Fibonacci series to be printed</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Enter the length of Fibonacci series: ");
-         int end = int.Parse(Console.ReadLine());
-         Console.WriteLine("\n------Fibonacci Series------");
-         int count = 2, i = 0, j = 1;
-         Console.Write ($"{i} {j}");
-         while (count <= end) {
-            int result = i + j;
-            Console.Write ($" {result}");
-            i = j; j = result;
-            count++;
+         for(; ; ) {
+            Console.Write ("Enter the length of Fibonacci series: ");
+            if (int.TryParse (Console.ReadLine (), out int end)) {
+               Console.WriteLine ("\n------Fibonacci Series------");
+               for (int i = 0; i < end; i++) Console.Write ($" {Fibonacci (i)} "); break;
+            } else continue;
          }
-         Console.WriteLine ();
       }
       #endregion
+      /// <summary>Gives the elements of Fibonacci series</summary>
+      /// <param name="n">Number of element in Fibonacci series</param>
+      /// <returns>Returns nth element of Fibonacci series</returns>
+      static int Fibonacci (int n) {
+         int[] fib = new int[n];
+         switch (n) {
+            case 0: return 0;
+            case 1: return 1;
+            default: return Fibonacci (n - 1) + Fibonacci (n - 2);
+         }
+      }
    }
    #endregion
 }
