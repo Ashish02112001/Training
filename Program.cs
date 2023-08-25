f62892745a30f85807ecb274ab3d6f0d04f58b76
Ashish <ashishkumar.m.b@metamation.com>
2023-08-25 10:55:00 +0530

    Improved logic in T05 - Prime number check

--- Program.cs
+++ Program.cs
@@ -5,21 +5,23 @@
 // Program.cs
 // program takes a number as input and displays whether it is a prime number or not.
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Prime number checker</summary>
    internal class Program {
       #region Methods ---------------------------------------------
       /// <summary>Gets an number from a user and checks it's a prime number</summary>
-      /// <param name="args">arguements</param>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.Write ("Enter a number: ");
-         int num = int.Parse (Console.ReadLine ()), count = 0;
-         for (int i = 1; i <= num; i++) if (num % i == 0) count++;
-         if (count == 2) Console.WriteLine ($"{num} is a Prime number");
-         else Console.WriteLine ($"{num} is a not a Prime number");
+         for(; ; ) {
+            Console.Write ("Enter a number: ");
+            int count = 0;
+            if (int.TryParse (Console.ReadLine (), out int num)) {
+               for (int i = 2; i < num; i++) if (num % i == 0) { count++; break; }
+               if (count == 1) Console.WriteLine ($"{num} is not a Prime number");
+               else Console.WriteLine ($"{num} is a Prime number");break;
+            }else continue;
+         }
       }
       #endregion
    }
