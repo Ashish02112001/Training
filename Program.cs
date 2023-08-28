f964a1fe036204f91f781fcab3559c671000e085
Ashish <ashishkumar.m.b@metamation.com>
2023-08-28 15:02:27 +0530

    Created T14 - Reduced string

--- Program.cs
+++ Program.cs
@@ -3,19 +3,26 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// Reduce a string of lowercase characters by deleting a pair of adjacent letters that match. 
+// Input: aaabccddd, Output: abd
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Reduced string</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
-      /// <param name="args">arguements</param>
+      /// <summary>This Method prints the reduced string for a given string</summary>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int i = 0;
+         for(; ; ) {
+            Console.WriteLine ("Enter a word: ");
+            string input = Console.ReadLine ();
+            if (input.All (char.IsAsciiLetterLower)) {
+               while (i < input.Length - 1) if (input[i] == input[i + 1]) { input = input.Remove (i, 2); } else i++;
+               Console.WriteLine ($"Reduced String: {input}"); break;
+            } else Console.WriteLine ("Try again with lower case alphabets");continue;
+         }
       }
       #endregion
    }
