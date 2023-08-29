883735aebe2680acf3f561d4aed82dcdc93c62f2
Ashish <ashishkumar.m.b@metamation.com>
2023-08-29 16:45:32 +0530

    Created T16 - Longest Abecedarian word

--- Program.cs
+++ Program.cs
@@ -3,21 +3,37 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// ABECEDERIAN word
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Sample program</summary>
    internal class Program {
       #region Methods ---------------------------------------------
       /// <summary>This Method prints "Hello, World!"</summary>
-      /// <param name="args">arguements</param>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         string[] words = {"AEGILOPS","BILLOWY","ALMOST","level","Habits" };
+         Console.WriteLine($"Given Words: ");
+         foreach (string word in words) { Console.Write ($"{word} "); }
+         Console.WriteLine ($"\nLongest ABECEDERIAN word: {LongAbeced (words)}");
       }
       #endregion
+      static string LongAbeced (string[] words) {
+         List<string> wordList = new();
+         int count = 0;
+         foreach (string word in words) {
+            if (Abcedarian (word)) { wordList.Add (word); count++; }
+            else wordList.Add (" ");
+         }
+         return wordList.OrderByDescending (a => a.Length).First ();
+      }
+      static bool Abcedarian(string word) {
+         bool valid = false;
+         word = word.ToUpper();
+         for (int i = 0;i<word.Length-1;i++) { if (word[i] <= word[i + 1]) { valid = true; } else { valid = false; break; } }
+         return valid;
+      }
    }
    #endregion
 }
