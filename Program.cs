d56470b9ba175397ef2c4b28b60b76456eb55a55
Ashish <ashishkumar.m.b@metamation.com>
2023-09-02 11:45:50 +0530

    Corrected comments in T11 - Digital root of a number

--- Program.cs
+++ Program.cs
@@ -5,28 +5,26 @@
 // Program.cs
 //  program to find the digital root of a given number
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Palindrome check for integers</summary>
+   /// <summary>Digital root</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method gets integer and checks it's a palindrome or not</summary>
-      /// <param name="args">arguements</param>
+      /// <summary>This Method gets number to find it's digital root</summary>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
          Console.Write ("Enter a Number: ");
          int num = int.Parse (Console.ReadLine());
          Console.WriteLine ($"Digital root of {num} is {digitalRoot(num)}");
       }
       #endregion
-      /// <summary>This function gets the integer and calculte it's digital root</summary>
+      /// <summary>This calcultes digital root of a number</summary>
       /// <param name="num">Number from the user</param>
       /// <returns>Gives digital root of the number</returns>
       static int digitalRoot(int num) {
-         int digit = 0, sum = 0;
+         int sum = 0;
          while (num > 0) {
-            digit = num % 10;
+            int digit = num % 10;
             num /= 10;
             sum += digit;
          }
