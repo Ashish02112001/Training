70310c5cb7c397805e949e7f4ac18960f557c809
Ashish <ashishkumar.m.b@metamation.com>
2023-09-05 12:38:48 +0530

    Completed T19 - Factorial of a number

--- Program.cs
+++ Program.cs
@@ -3,21 +3,26 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// Function to print the factorial for the given number
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Factorial of a number</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
-      /// <param name="args">arguements</param>
+      /// <summary>This method asks the number from the user and gives factorial of it</summary>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.Write ("Enter a Number: ");
+         for(; ; ) {
+            if (int.TryParse (Console.ReadLine (), out var num)) {
+               Console.WriteLine ($"{num}! = {Factorial (num)}");
+               break;
+            } else Console.Write ("Enter a valid number: ");
+         }
       }
       #endregion
+      static int Factorial (int num) => (num > 1) ? num * Factorial (num - 1) : 1;
    }
    #endregion
 }
