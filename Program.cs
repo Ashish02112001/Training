9a65bdff17ed5f1b91da82e77d4f028d41c8fefc
Ashish <ashishkumar.m.b@metamation.com>
2023-09-11 16:01:09 +0530

    Cached the contents of Armstrong numbers in Cache.txt

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
 // Write a function to determine the nth Armstrong number input index that needs to be passed as a command-line argument to the application.
 // For example, Armstrong.exe 12 should print as 371. Maximum input can be restricted to 25. 
 // --------------------------------------------------------------------------------------------
+using System.IO;
+
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Nth Armstrong Number</summary>
@@ -17,48 +19,64 @@ namespace Training {
       static void Main (string[] args) {
          for (; ; ) {
             Console.Write ("Enter Nth term of Armstrong number to be printed: ");
-            if (int.TryParse (Console.ReadLine (), out var num) && num <= 25) {
-               Console.WriteLine ($"{num}th term of Armstrong number is {ReadFile (num)}"); break;
+            if (int.TryParse (Console.ReadLine (), out var num) ) {
+               Console.WriteLine ($"{num}th term of Armstrong number is {NthArmstrong (num)}"); break;
             } else {
-               Console.WriteLine ("Invalid input. Please enter a positive integer below 25");
+               Console.WriteLine ("Invalid input. Please enter a positive integer");
             }
          }
       }
-      static int ReadFile (int nth) {
+      static string NthArmstrong (int nth) {
          string path = "W:/Cache.txt";
          if (File.Exists (path)) {
-            string str = File.ReadAllText (path);
-            if(nth <= str.Trim ().Length) foreach (int n in str) return (str[nth-1]-48);
-            else { NthArmstrong (nth); return 0; }
-         }
-         return 0;
+            List<string> nums = File.ReadAllLines (path).ToList();
+            if (nth < nums.Count) return nums[nth - 1];
+            else { return $"{CalculateNthArmstrong (nth)}"; }
+         } else { FileStream fs = File.Create ("C:/Cache.txt"); return $"{CalculateNthArmstrong (nth)}"; };
       }
-         #endregion
-         /// <summary>This method prints the Nth Armstrong number</summary>
-         /// <param name="nthNum">Nth term</param>
-         /// <returns>Nth term of Armstrong number</returns>
-      static int NthArmstrong (int nthNum) {
-         int count = 0, num = 1;
+      static void AppendNthArmsToCache(int num) {
+         string path = "W:/Cache.txt";
+         string n = num.ToString ();
+         List<string> nums = File.ReadAllLines (path).ToList();
+         using StreamWriter sw = File.AppendText(path);
+         if (!nums.Contains(n))sw.WriteLine (num);
+      }
+      /// <summary>This method prints the Nth Armstrong number</summary>
+      /// <param name="nthNum">Nth term</param>
+      /// <returns>Nth term of Armstrong number</returns>
+      static int CalculateNthArmstrong (int nthNum) {
+         string path = "W:/Cache.txt";
+         List<string> nums = File.ReadAllLines (path).ToList ();
+         int count, num;
+         if (nums.Count > 0) {
+            count = nums.Count;
+            num = int.Parse(nums.Last());
+         }else {count = 0;num = 1; }
          while (count <= nthNum) {
-            if (Armstrong (num)) { count++; if (count == nthNum) { break; } }
+            if (Armstrong (num)) { 
+               count++;
+               AppendNthArmsToCache (num); 
+               if (count == nthNum) { break; } 
+            }
             num++;
          }
          return num;
       }
-         /// <summary>Checks the number is Armstrong number or not</summary>
-         /// <param name="n">Number to be checked</param>
-         /// <returns>True if it is Armstrong number else false</returns>
-         static bool Armstrong (int n) {
-            double sum = 0, pow = n.ToString ().Length;
-            int nCpy = n;
-            while (nCpy > 0) {
-               int digit = nCpy % 10;
-               sum += Math.Pow (digit, pow);
-               nCpy /= 10;
-            }
-            return sum == n;
+      /// <summary>Checks the number is Armstrong number or not</summary>
+      /// <param name="n">Number to be checked</param>
+      /// <returns>True if it is Armstrong number else false</returns>
+      static bool Armstrong (int n) {
+         double sum = 0, pow = n.ToString ().Length;
+         int nCpy = n;
+         while (nCpy > 0) {
+            int digit = nCpy % 10;
+            sum += Math.Pow (digit, pow);
+            nCpy /= 10;
          }
-      
+         return sum == n;
+      }
       #endregion
    }
+   #endregion
+
 }
