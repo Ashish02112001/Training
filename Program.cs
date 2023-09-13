51f92ebfd635d94857e7a8eb1f992a5cf37a8d80
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-09-13 10:55:00 +0530

    Made Corrections

--- Program.cs
+++ Program.cs
@@ -9,39 +9,36 @@
 // --------------------------------------------------------------------------------------------
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Abecedarian word</summary>
+   /// <summary>Longest Abecedarian word</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>Given Abecedarian words it returns the longest one</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         string[] words = {"AEGILOPS","BILLOWY","ALMOST","level","Habits" };
-         Console.WriteLine($"Given Words: ");
-         foreach (string word in words) { Console.Write ($"{word} "); }
+         string[] words = { "AEGILOPS", "BILLOWY", "ALMOST", "level", "Habits" };
+         Console.WriteLine ($"Given Words: ");
+         foreach (string word in words) Console.Write ($"{word} ");
          Console.WriteLine ($"\nLongest ABECEDERIAN word: {LongAbeced (words)}");
       }
-      #endregion
+
       /// <summary>This method takes the string array and returns the longest Abecederian word</summary>
       /// <param name="words">Given array of strings</param>
       /// <returns>Longest Abcecederian word in the array</returns>
       static string LongAbeced (string[] words) {
-         List<string> wordList = new();
-         int count = 0;
-         foreach (string word in words) {
-            if (Abcedarian (word)) { wordList.Add (word); count++; }
-            else wordList.Add (" ");
-         }
-         return wordList.OrderByDescending (a => a.Length).First ();
+         List<string> wordList = new ();
+         foreach (string word in words) if (IsAbcedarian (word)) wordList.Add (word);
+         return wordList.OrderByDescending (a => a.Length).FirstOrDefault () ?? "";
       }
+
       /// <summary>This method cheks the word is a Abecedarian or not</summary>
       /// <param name="word">Word</param>
       /// <returns>Returns true if the word is Abecedarian else false</returns>
-      static bool Abcedarian(string word) {
-         bool valid = false;
-         word = word.ToUpper();
-         for (int i = 0;i<word.Length-1;i++) { if (word[i] <= word[i + 1]) { valid = true; } else { valid = false; break; } }
-         return valid;
+      static bool IsAbcedarian (string word) {
+         word = word.ToUpper ();
+         for (int i = 0; i < word.Length - 1; i++) if (word[i] <= word[i + 1]) continue; else { return false; } 
+         return true;
       }
+      #endregion
    }
    #endregion
 }
