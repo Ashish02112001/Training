2f873a2d98be5d51d306e2163d7c93c63ab021cf
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-09-27 17:42:28 +0530

    T23 Incomplete

--- Program.cs
+++ Program.cs
@@ -3,39 +3,38 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Given a string S to a method, with each character in it representing a vote for a contestant, return the winner with the most votes.
-// If 2 or more contestants have the same number of votes, the contestant who got the first vote among them is declared a winner. 
-// Input Criteria: Character representation is Case-Insensitive and Alphabet. S should not be empty. 
-// Use at least 1 keyword/technique: in, out, ref, optional parameters, multiple return values. 
+// Write a method, for a given money X along with price P of a chocolate and required wrappers W for a chocolate in exchange,
+// return the maximum number of chocolates C you can get along with any unused money X and wrappers W. 
 // --------------------------------------------------------------------------------------------
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Voting Vontest</summary>
+   /// <summary>Chocolate Wrappers</summary>
    internal class Program {
       #region Methods ---------------------------------------------
       /// <summary>Gets the votes and gives the winner</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         string S;
-         for (; ; ) {
-            Console.Write ("Enter your vote for contestants(A to Z): ");
-            S = Console.ReadLine () ?? "";
-            if (S.All (char.IsAsciiLetter) && S.Length > 0) {
-               var majority = VotesCounter (S);
-               Console.WriteLine ($"The winner is {majority.ch} with {majority.n} votes");
-               break;
-            }
-         }
+         int X = 30, P = 5, W = 5;
+         Console.WriteLine (ChocoWrappers(X,P,W));
+         //for (; ; ) {
+         //   Console.Write ("Enter your vote for contestants(A to Z): ");
+         //   S = Console.ReadLine () ?? "";
+         //   if (S.All (char.IsAsciiLetter) && S.Length > 0) {
+         //      var majority = VotesCounter (S);
+         //      Console.WriteLine ($"The winner is {majority.ch} with {majority.n} votes");
+         //      break;
+         //   }
+         //}
       }
-      /// <summary>Gets the user input and returns the winner and their vcount</summary>
-      /// <param name="votes">User input</param>
-      /// <returns>winner and vote count</returns>
-      static (char ch, int n) VotesCounter (string votes) {
-         votes = votes.ToLower ();
-         Dictionary<char, int> votesCounter = new Dictionary<char, int> ();
-         foreach (char vote in votes) votesCounter[vote] = votesCounter.TryGetValue (vote, out int voteCount) ? ++voteCount : 1;
-         var winner = votesCounter.OrderByDescending (x => x.Value).First ();
-         return (winner.Key, winner.Value);
+      /// <summary></summary>
+      /// <param name="X"></param>
+      /// <param name="P"></param>
+      /// <param name="W"></param>
+      /// <returns></returns>
+      static (int C, int X, int W) ChocoWrappers (int X, int P,int W) {
+         int wraperchocoCount = (X / P) - W;
+         return (wraperchocoCount > 0) ? ChocoWrappers((X/P),W,wraperchocoCount) : (X+W, )
+         //return (wraperchocoCount > 0) ? ((X / P) + wraperchocoCount, X % P, X % (X / P)) : ((X / P), X % P, 1);
       }
       #endregion
    }
