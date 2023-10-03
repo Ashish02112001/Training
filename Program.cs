cf1ebb3cba545c31bcd96544f618655c91b00643
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-03 11:54:41 +0530

    T25 Incomplete

--- Program.cs
+++ Program.cs
@@ -15,7 +15,17 @@ namespace Training {
       /// <summary>This Method prints "Hello, World!"</summary>
       /// <param name="args">arguements</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.WriteLine ("Enter number of elements: ");
+         string letters = Console.ReadLine () ?? "";
+         char[] A = letters.ToCharArray ();
+         Console.WriteLine ("Enter the special character: ");
+         char S = (char)Console.Read();
+         Console.WriteLine ("Enter the sort order(Ascending or Descending): ");
+         string O = Console.ReadLine() ?? "";
+         A = O == "descending" ? A.OrderByDescending(x => x).ToArray() : A.Order().ToArray();
+         Array.Resize(ref A, A.Length + 1);
+         A[A.Length-1] = S;
+         foreach (char ch in A) Console.Write ($"{ch}, ");
       }
       #endregion
    }
