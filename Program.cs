9e90adb0d8be8ffc050dc68872c73871d97635fc
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-04 16:16:54 +0530

    Implemented using a method

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// Given a character array A, along with special character S and sort order O (default order is Ascending),
+// print the sorted array by keeping the elements matching S to the last of the array. 
 // --------------------------------------------------------------------------------------------
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Sort and Swap special character</summary>
    internal class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This method sorts the character array and adds the special character at the last</summary>
+      /// <summary>This method gets the character array, Special Character and Order then prints sorted and swapped string</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
          Console.Write ("Enter the elements of character array as a word: ");
@@ -19,11 +20,20 @@ namespace Training {
          char S = Console.ReadLine ().First ();
          Console.WriteLine ("Enter the sort order(Ascending or Descending): ");
          string O = Console.ReadLine () ?? "".ToLower ();
-         string filterA = "", sp = "";
-         foreach (char c in A) if (c == S) sp += c; else filterA += c;
-         char[] sp1 = filterA.ToCharArray ();
-         sp1 = ((O == "descending") ? sp1.OrderDescending () : sp1.Order ()).ToArray ();
-         Console.WriteLine ($"{string.Join (",", sp1)} {string.Join (",", sp.ToArray ())}");
+         Console.WriteLine ($"Sorted and swapped special characrter: [{SortAndSwapSplChr (A, S, O)}]");
+      }
+      /// <summary>This method sorts the character array and adds the special character at the last</summary>
+      /// <param name="A">Given character array</param>
+      /// <param name="S">Special Character</param>
+      /// <param name="O">Order of sort</param>
+      /// <param name="filterA">optional variable to store the letters excluding special characters</param>
+      /// <param name="spChr">optional variable to store special characters from a given array</param>
+      /// <returns>Sorted and swapped string seperated by ","</returns>
+      static string SortAndSwapSplChr (char[] A, char S, string O, string filterA = "", string spChr = "") {
+         foreach (char c in A) if (c == S) spChr += c; else filterA += c;
+         char[] filterArr = filterA.ToCharArray ();
+         filterArr = ((O == "descanding") ? filterArr.OrderDescending () : filterArr.Order ()).ToArray ();
+         return ($"{string.Join (",", filterArr)},{string.Join (",", spChr.ToArray ())}");
       }
       #endregion
    }
