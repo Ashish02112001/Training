53fc78bb264fde1bb7b5393412665f8c078fb1c0
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-04 09:59:59 +0530

    Completed T25

--- Program.cs
+++ Program.cs
@@ -5,27 +5,45 @@
 // Program.cs
 // Simple Program to Print the text 'Hello, World!' in Console
 // --------------------------------------------------------------------------------------------
-
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>Sort and Swap special character</summary>
    internal class Program {
       #region Methods ---------------------------------------------
       /// <summary>This Method prints "Hello, World!"</summary>
-      /// <param name="args">arguements</param>
+      /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Enter number of elements: ");
-         string letters = Console.ReadLine () ?? "";
-         char[] A = letters.ToCharArray ();
-         Console.WriteLine ("Enter the special character: ");
-         char S = (char)Console.Read();
+         Console.Write ("Enter the elements of character array as a word: ");
+         char[] letters = (Console.ReadLine () ?? "").ToCharArray ();
+         //char[] A = letters
+         Console.Write ("Enter the special character: ");
+         char S = Console.ReadLine ().First();
          Console.WriteLine ("Enter the sort order(Ascending or Descending): ");
-         string O = Console.ReadLine() ?? "";
-         A = O == "descending" ? A.OrderByDescending(x => x).ToArray() : A.Order().ToArray();
-         Array.Resize(ref A, A.Length + 1);
-         A[A.Length-1] = S;
-         foreach (char ch in A) Console.Write ($"{ch}, ");
+         string O = Console.ReadLine () ?? "".ToLower ();
+         //char[] filterA = new char[A.Length];
+         string filterA = "";
+         string sp = "";
+         //foreach (char c in letters) { if (c != S) filterA = filterA.Append (c).ToArray (); }
+         foreach (char c in letters)
+            if (c == S)
+               sp += c;
+            else filterA += c;
+         char[] sp1 = filterA.ToCharArray();
+         sp1 = ((O == "descending") ? sp1.OrderDescending () : sp1.Order ()).ToArray();
+
+         //int sCnt = 2;
+         //for (int i = 0; i < sCnt; i++) filterA[^1] = S;
+         //int sCnt = 0;
+         //if (A.Contains (S)) {
+         //   for (int i = 0; i < A.Length-1; i++) {
+         //      if (A[i] == S) {
+         //         A = A[..i].Concat (A[(i + 1)..]).ToArray ();
+         //         Array.Resize (ref A, A.Length + 1);
+         //         A[^1] = S;
+         //      }
+         //   }
+         //}
+         Console.WriteLine ($"{string.Join (",", sp1)} {string.Join(",",sp.ToArray())}");
       }
       #endregion
    }
