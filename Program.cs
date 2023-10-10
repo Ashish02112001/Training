98e5ee0f1a13b40d77c251fa4abf8593358b56dc
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-10 16:01:26 +0530

    Comments and Exceptions are yet to be added

--- Program.cs
+++ Program.cs
@@ -28,16 +28,19 @@ namespace Training {
       }
       #endregion
    }
-
+   /// <summary>Custom List</summary>
+   /// <typeparam name="T">list type</typeparam>
    class MyList<T> {
       T[] elements;
       int size = 0;
       int capacity;
+      /// <summary></summary>
+      /// <param name="initCap"></param>
       public MyList (int initCap = 4) {
          capacity = initCap;
          elements = new T[initCap];
       }
-
+      /// <summary></summary>
       public int Count => size;
       public int Capacity => capacity;
       public T this[int index] {
@@ -46,6 +49,8 @@ namespace Training {
             elements[index] = value;
          }
       }
+      /// <summary>Adds a specified element to the list</summary>
+      /// <param name="a"></param>
       public void Add (T a) {
          if (size == capacity) {
             Array.Resize (ref elements, capacity * 2);
@@ -53,6 +58,9 @@ namespace Training {
          }
          elements[size++] = a;
       }
+      /// <summary>Removes a specified element from the list</summary>
+      /// <param name="a">element to be removed from the list</param>
+      /// <returns>returns true if the element is present in the list otherwise false</returns>
       public bool Remove (T a) {
          if (elements.Contains (a)) {
             for (int i = Array.IndexOf (elements, a); i < size - 1; i++) elements[i] = elements[i + 1];
@@ -60,15 +68,21 @@ namespace Training {
             return true;
          } else return false;
       }
+      /// <summary>Clears all the elements in the list</summary>
       public void Clear () { 
          Array.Clear (elements, 0, size);
          size = 0;
       }
+      /// <summary>Inserts a given element at a specified index</summary>
+      /// <param name="index">Index at which the element has to be insereted</param>
+      /// <param name="a">element to be inserted in the list</param>
       public void Insert (int index, T a) {
          size++;
          for (int i = size-1; i >= index+1; i--) elements[i] = elements[i - 1];
          elements[index] = a;   
       }
+      /// <summary>Removes the element at the specified index</summary>
+      /// <param name="index">Index at which the element has to be removed</param>
       public void RemoveAt (int index) {
          if (index < size) {
             for (int i = index; i < size - 1; i++) elements[i] = elements[i + 1];
