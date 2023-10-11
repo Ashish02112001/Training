900fc13fb63c716e919ff7226b8f675bd0327b6a
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-11 16:04:12 +0530

    Completed MyClass - Testing is pending

--- Program.cs
+++ Program.cs
@@ -3,28 +3,39 @@
 // Copyright (c) Metamation India.                                              
 // ------------------------------------------------------------------------
 // Program.cs
-// Simple Program to Print the text 'Hello, World!' in Console
+// Implement a custom MyList<T> class using arrays as the underlying data structure.
+// The MyList<T> should start with an initial capacity of 4 and double its capacity when needed.
+// Throw exceptions when necessary. 
 // --------------------------------------------------------------------------------------------
 namespace Training {
    #region Program ------------------------------------------------------------------------------
-   /// <summary>Sample program</summary>
+   /// <summary>MyList<T></summary>
    class Program {
       #region Methods ---------------------------------------------
-      /// <summary>This Method prints "Hello, World!"</summary>
+      /// <summary>Tests the custom MyList<T></summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         MyList<int> nums = new ();
-         nums.Add (1);
-         nums.Add (3);
-         nums.Add (4);
-         nums.Add (5);
-         nums.Add (6);
-         nums.Add (7);
-         nums.Insert(1, 2);
-         nums.Insert (7, 8);
-         nums.RemoveAt(4);
-         for (int i = 0; i < nums.Count; i++) { Console.WriteLine (nums[i]); }
-         //Console.WriteLine (nums.Count);
+         MyList<string> seaStory = new ();
+         Console.WriteLine ("Adding Elements to Mylist(seaStory)...");
+         seaStory.Add ("boat");
+         seaStory.Add ("star");
+         seaStory.Add ("sea");
+         seaStory.Add ("pirate");
+         seaStory.Add ("Yatch");
+         Console.WriteLine ("Printing elements in the seaStory...");
+         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
+         Console.WriteLine ($"Count: {seaStory.Count}");
+         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
+         Console.WriteLine ("Removing 'Yatch' from the list");
+         seaStory.Remove ("Yatch");
+         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
+         Console.WriteLine ($"Count: {seaStory.Count}");
+         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
+         Console.WriteLine ("Removing 1st index element from the list");
+         seaStory.RemoveAt(1);
+         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
+         Console.WriteLine ($"Count: {seaStory.Count}");
+         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
       }
       #endregion
    }
@@ -34,23 +45,33 @@ namespace Training {
       T[] elements;
       int size = 0;
       int capacity;
-      /// <summary></summary>
-      /// <param name="initCap"></param>
+
+      /// <summary>Constructor for the class MyList</summary>
+      /// <param name="initCap">Initial capacity for the list</param>
       public MyList (int initCap = 4) {
          capacity = initCap;
          elements = new T[initCap];
       }
-      /// <summary></summary>
+      /// <summary>returns the number of elements in the list</summary>
       public int Count => size;
+      /// <summary>returns the current limit of the list</summary>
       public int Capacity => capacity;
+      /// <summary>This assigns the value to a specified index or to return the element of the specified index in a list</summary>
+      /// <param name="index">index of the list</param>
+      /// <returns>element of the specific index in a list</returns>
+      /// <exception cref="Exception">Index out of the range exception</exception>
       public T this[int index] {
-         get => elements[index];
+         get {
+            if (index >= size || index < 0) throw new Exception ("Index was outside the bounds of the List.");
+            else return elements[index];
+         }
          set {
+            if (index > Capacity || index < 0) throw new Exception ("Index was outside the bounds of the List.");
             elements[index] = value;
          }
       }
       /// <summary>Adds a specified element to the list</summary>
-      /// <param name="a"></param>
+      /// <param name="a">Element to be added inside the list</param>
       public void Add (T a) {
          if (size == capacity) {
             Array.Resize (ref elements, capacity * 2);
@@ -66,10 +87,10 @@ namespace Training {
             for (int i = Array.IndexOf (elements, a); i < size - 1; i++) elements[i] = elements[i + 1];
             elements[size-- - 1] = default;
             return true;
-         } else return false;
+         } else throw new InvalidOperationException ();
       }
       /// <summary>Clears all the elements in the list</summary>
-      public void Clear () { 
+      public void Clear () {
          Array.Clear (elements, 0, size);
          size = 0;
       }
@@ -77,19 +98,19 @@ namespace Training {
       /// <param name="index">Index at which the element has to be insereted</param>
       /// <param name="a">element to be inserted in the list</param>
       public void Insert (int index, T a) {
+         if (index > size) { throw new Exception ("Index was outside the bounds of the List."); }
+         for (int i = size - 1; i >= index + 1; i--) elements[i] = elements[i - 1];
+         elements[index] = a;
          size++;
-         for (int i = size-1; i >= index+1; i--) elements[i] = elements[i - 1];
-         elements[index] = a;   
+
       }
       /// <summary>Removes the element at the specified index</summary>
       /// <param name="index">Index at which the element has to be removed</param>
       public void RemoveAt (int index) {
-         if (index < size) {
-            for (int i = index; i < size - 1; i++) elements[i] = elements[i + 1];
-            elements[size-- - 1] = default;
-         } 
+         if (index > size) { throw new Exception ("Index was outside the bounds of the List."); }
+         for (int i = index; i < size - 1; i++) elements[i] = elements[i + 1];
+         elements[size-- - 1] = default;
       }
-         
    }
    #endregion
 }
