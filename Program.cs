528d758786d2e52d0989a0bb08ff96e3a9ee6dd3
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-12 17:54:00 +0530

    Completed MyList

--- Program.cs
+++ Program.cs
@@ -23,94 +23,107 @@ namespace Training {
          seaStory.Add ("pirate");
          seaStory.Add ("Yatch");
          Console.WriteLine ("Printing elements in the seaStory...");
-         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
-         Console.WriteLine ($"Count: {seaStory.Count}");
-         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
-         Console.WriteLine ("Removing 'Yatch' from the list");
-         seaStory.Remove ("Yatch");
-         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
-         Console.WriteLine ($"Count: {seaStory.Count}");
-         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
+         PrintElementsAndCount(seaStory);
+         Console.WriteLine ("Removing the element \"Sea\" in the list");
+         seaStory.Remove ("sea");
+         PrintElementsAndCount (seaStory);
          Console.WriteLine ("Removing 1st index element from the list");
-         seaStory.RemoveAt(1);
-         for (int i = 0; i < seaStory.Count; i++) Console.WriteLine ("\t" + seaStory[i]);
-         Console.WriteLine ($"Count: {seaStory.Count}");
-         Console.WriteLine ($"Capacity: {seaStory.Capacity}");
+         seaStory.RemoveAt (1);
+         PrintElementsAndCount (seaStory);
+         Console.WriteLine("Inserting the element \"sea\" at the 2nd index");
+         seaStory.Insert (2,"sea");
+         PrintElementsAndCount (seaStory);
+         Console.WriteLine ("Clearing all elements");
+         seaStory.Clear();
+         PrintElementsAndCount(seaStory);
+      }
+      static void PrintElementsAndCount (MyList<string> list) {
+         for (int i = 0; i < list.Count; i++) Console.WriteLine ("\t" + list[i]);
+         Console.WriteLine ($"Count: {list.Count}");
+         Console.WriteLine ($"Capacity: {list.Capacity}");
       }
       #endregion
    }
+
    /// <summary>Custom List</summary>
    /// <typeparam name="T">list type</typeparam>
+   #region MyList_Class ------------------------------------------------------------------------------
    class MyList<T> {
-      T[] elements;
-      int size = 0;
-      int capacity;
-
       /// <summary>Constructor for the class MyList</summary>
       /// <param name="initCap">Initial capacity for the list</param>
       public MyList (int initCap = 4) {
-         capacity = initCap;
-         elements = new T[initCap];
+         mSize = 0;
+         mCapacity = initCap;
+         mElements = new T[initCap];
       }
+      #region Properties-----------------------------------------------
       /// <summary>returns the number of elements in the list</summary>
-      public int Count => size;
+      public int Count => mSize;
       /// <summary>returns the current limit of the list</summary>
-      public int Capacity => capacity;
+      public int Capacity => mCapacity;
+
       /// <summary>This assigns the value to a specified index or to return the element of the specified index in a list</summary>
       /// <param name="index">index of the list</param>
       /// <returns>element of the specific index in a list</returns>
       /// <exception cref="Exception">Index out of the range exception</exception>
       public T this[int index] {
          get {
-            if (index >= size || index < 0) throw new Exception ("Index was outside the bounds of the List.");
-            else return elements[index];
+            if (index >= mSize || index < 0) throw new Exception ("Index was outside the bounds of the List.");
+            else return mElements[index];
          }
          set {
             if (index > Capacity || index < 0) throw new Exception ("Index was outside the bounds of the List.");
-            elements[index] = value;
+            mElements[index] = value;
          }
       }
+      #endregion
+      #region Methods---------------------------------------------
       /// <summary>Adds a specified element to the list</summary>
       /// <param name="a">Element to be added inside the list</param>
       public void Add (T a) {
-         if (size == capacity) {
-            Array.Resize (ref elements, capacity * 2);
-            capacity *= 2;
+         if (mSize == mCapacity) {
+            Array.Resize (ref mElements, mCapacity * 2);
+            mCapacity *= 2;
          }
-         elements[size++] = a;
+         mElements[mSize++] = a;
       }
       /// <summary>Removes a specified element from the list</summary>
       /// <param name="a">element to be removed from the list</param>
       /// <returns>returns true if the element is present in the list otherwise false</returns>
       public bool Remove (T a) {
-         if (elements.Contains (a)) {
-            for (int i = Array.IndexOf (elements, a); i < size - 1; i++) elements[i] = elements[i + 1];
-            elements[size-- - 1] = default;
+         if (mElements.Contains (a)) {
+            for (int i = Array.IndexOf (mElements, a); i < mSize - 1; i++) mElements[i] = mElements[i + 1];
+            mElements[mSize-- - 1] = default;
             return true;
          } else throw new InvalidOperationException ();
       }
       /// <summary>Clears all the elements in the list</summary>
       public void Clear () {
-         Array.Clear (elements, 0, size);
-         size = 0;
+         Array.Clear (mElements, 0, mSize);
+         mSize = 0;
       }
       /// <summary>Inserts a given element at a specified index</summary>
       /// <param name="index">Index at which the element has to be insereted</param>
       /// <param name="a">element to be inserted in the list</param>
       public void Insert (int index, T a) {
-         if (index > size) { throw new Exception ("Index was outside the bounds of the List."); }
-         for (int i = size - 1; i >= index + 1; i--) elements[i] = elements[i - 1];
-         elements[index] = a;
-         size++;
+         if (index > mSize) { throw new Exception ("Index was outside the bounds of the List."); }
+         mSize++;
+         for (int i = mSize - 1; i >= index + 1; i--) mElements[i] = mElements[i - 1];
+         mElements[index] = a;
 
       }
       /// <summary>Removes the element at the specified index</summary>
       /// <param name="index">Index at which the element has to be removed</param>
       public void RemoveAt (int index) {
-         if (index > size) { throw new Exception ("Index was outside the bounds of the List."); }
-         for (int i = index; i < size - 1; i++) elements[i] = elements[i + 1];
-         elements[size-- - 1] = default;
+         if (index > mSize) { throw new Exception ("Index was outside the bounds of the List."); }
+         for (int i = index; i < mSize - 1; i++) mElements[i] = mElements[i + 1];
+         mElements[mSize-- - 1] = default;
       }
+      #endregion
+      T[] mElements;
+      int mSize;
+      int mCapacity;
    }
+   #endregion
    #endregion
 }
