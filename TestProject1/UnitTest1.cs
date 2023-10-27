c9e0bf52564306e8cf24180d5570da82e59f2772
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2023-10-27 11:05:39 +0530

    Created seperate Training and Test folder

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,65 @@
+using Training;
+namespace TestProject1 {
+   [TestClass]
+   public class UnitTest1 {
+
+      List<int> nums = new ();
+      MyList<int> myNums = new ();
+
+      [TestMethod]
+      public void TestAdd () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (2); myNums.Add (2);
+         nums.Add (3); myNums.Add (3);
+         Assert.AreEqual (nums[0], myNums[0]);
+         nums[0] = 1; myNums[0] = 1;
+         Assert.AreEqual (nums[0], myNums[0]);
+         nums.Clear (); myNums.Clear ();
+      }
+      [TestMethod]
+      public void TestRemove () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (2); myNums.Add (2);
+         nums.Remove (2); Assert.IsTrue (myNums.Remove (2));
+         Assert.AreEqual (nums.Count, myNums.Count);
+         Assert.IsFalse (myNums.Remove (5));
+         Assert.ThrowsException<IndexOutOfRangeException> (() => myNums[4]);
+         nums.Clear (); myNums.Clear ();
+      }
+      [TestMethod]
+      public void TestInsert () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (3); myNums.Add (3);
+         nums.Insert (1, 2); myNums.Insert (1, 2);
+         Assert.AreEqual (nums[1], myNums[1]);
+         Assert.ThrowsException<IndexOutOfRangeException> (() => myNums.Insert (4, 5));
+         nums.Clear (); myNums.Clear ();
+      }
+      [TestMethod]
+      public void TestClear () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (2); myNums.Add (2);
+         nums.Clear (); myNums.Clear ();
+         Assert.AreEqual (0, myNums.Count);
+         nums.Clear (); myNums.Clear ();
+
+      }
+      [TestMethod]
+      public void TestRemoveAt () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (2); myNums.Add (2);
+         nums.RemoveAt (1); myNums.RemoveAt (1);
+         Assert.AreEqual (nums.Count, myNums.Count);
+         Assert.ThrowsException<IndexOutOfRangeException> (() => myNums.RemoveAt (4));
+         nums.Clear (); myNums.Clear ();
+      }
+      [TestMethod]
+      public void TestCapacity () {
+         nums.Add (1); myNums.Add (1);
+         nums.Add (2); myNums.Add (2);
+         nums.Add (3); myNums.Add (3);
+         nums.Add (4); myNums.Add (4);
+         Assert.AreEqual (8, myNums.Capacity);
+      }
+   }
+}
