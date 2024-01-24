892eb5eb3e13eda0a51962d22c21fb4945b04e67
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2024-01-24 11:17:30 +0530

    Incomplete game

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
 // Simple Program to Print the text 'Hello, World!' in Console
 // --------------------------------------------------------------------------------------------
 
+using System.Text;
+
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Sample program</summary>
@@ -14,8 +16,23 @@ namespace Training {
       /// <summary>This Method prints "Hello, World!"</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
       }
+      static void DisplayBoard () {
+         Console.OutputEncoding = Encoding.UTF8;
+         Console.WriteLine ("\t│\t│\n \t│\t│");
+         Console.WriteLine ("────────────────────────");
+         Console.WriteLine ("\t│\t│\n \t│\t│");
+         Console.WriteLine ("────────────────────────");
+         Console.WriteLine ("\t│\t│\n \t│\t│");
+      }
+      static void Game () {
+         for (int row = 0; row )
+         //while (inputs != 9) {
+         //   ConsoleKey key = Console.ReadKey (true).Key;
+            
+         //}
+      }
+      static int inputs { get; set; }
       #endregion
    }
    #endregion
