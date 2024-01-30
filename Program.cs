76c4d403e40a66b229ec59bcda00b1cf76a9897b
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2024-01-30 17:47:09 +0530

    Reverted the changes to remove unnecessary
    This reverts commit 33c76dc92b9cd9bc19cd50ea7b4d4b9d715c37f2.

--- Program.cs
+++ Program.cs
@@ -6,8 +6,6 @@
 // Simple Program to Print the text 'Hello, World!' in Console
 // --------------------------------------------------------------------------------------------
 
-using System.ComponentModel;
-
 namespace Training {
    #region Program ------------------------------------------------------------------------------
    /// <summary>Sample program</summary>
@@ -16,43 +14,9 @@ namespace Training {
       /// <summary>This Method prints "Hello, World!"</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         ComplexNumber z1 = new ComplexNumber (4, 3);
-         ComplexNumber z2 = new ComplexNumber (3, 4);
-         ComplexNumber z3 = new ComplexNumber (0, 0);
-         Console.WriteLine ($"Given Complex numbers: {z1.realPart} + {z1.imgPart}i and {z2.realPart} + {z2.imgPart}i");
-         Console.Write ($"Addition: ");
-         z3.Add (z1, z2);
-         Console.Write ($"\nSubtraction: ");
-         z3.Sub (z1, z2);
-         Console.WriteLine ($"\nMagnitude of 1st Complex number: {z1.Norm}");
+         Console.WriteLine ("Hello, World!");
       }
       #endregion
    }
    #endregion
-   public class ComplexNumber {
-      public ComplexNumber (int rPart, int iPart) {
-         realPart = rPart;
-         imgPart = iPart;
-      }
-      public readonly int realPart, imgPart;
-
-      public void Add (ComplexNumber z1, ComplexNumber z2) {
-         int realAdd = z1.realPart + z2.realPart;
-         int imgAdd = z1.imgPart + z2.imgPart;
-         Console.Write ($"{realAdd} + {imgAdd}i");
-      }
-      public void Sub (ComplexNumber z1, ComplexNumber z2) {
-         int realSub = z1.realPart - z2.realPart;
-         int imgSub = z1.imgPart - z2.imgPart;
-         char sign = imgPart > 0 ? '+' : '-';
-         if (sign == '-') imgSub *= -1;
-         Console.Write ($"{realSub} {sign} {imgSub}i");
-      }
-      public double Norm {
-         get {
-            double c = realPart*realPart + imgPart*imgPart;
-            return (Math.Sqrt (c));
-         }
-      }
-   }
 }
