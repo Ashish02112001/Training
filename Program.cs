c99f79fa3589b69d43028cce40b32cfe3944b202
Baalamuruganas <AshishKumar.Baalamurugan@trumpf.com>
2024-01-30 17:24:19 +0530

    Initial code running slowly

--- Program.cs
+++ Program.cs
@@ -14,9 +14,21 @@ namespace Training {
       /// <summary>This Method prints "Hello, World!"</summary>
       /// <param name="args">arguments</param>
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         string[] words = File.ReadAllLines ("Dict.txt");
+         Func<string, string> sortedWord = word => new string (word.ToCharArray ().OrderBy (x => x).ToArray ());
+         Dictionary<string, List<string>> anagrams = new ();
+         List<string> anagramChecked = new ();
+         foreach (string word in words) {
+            //if (anagramChecked.Contains ()) { }
+            List<string> list = words.Where (x => x != word && sortedWord (x) == sortedWord (word)).ToList ();
+            anagramChecked.Add (word);
+            if (list.Count > 0) anagrams.Add (word, list);
+         }
+         foreach (var anagram in anagrams.OrderByDescending (x => x.Value.Count)) {
+            Console.WriteLine ($"{anagram.Value.Count + 1} {anagram.Key} : {string.Join (",", anagram.Value)}");
+         }
       }
-      #endregion
    }
    #endregion
 }
+#endregion
